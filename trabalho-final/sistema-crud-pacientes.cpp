9d014132ecb04bcbdd5cbe5fbc11ebfd3be7a975
willian <34694258+willian-hm@users.noreply.github.com>
2026-06-18 11:40:48 +0000

    começo do trabalho final

---
+++ trabalho-final/sistema-crud-pacientes.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include MAX 100
+
+// Registro (struct) de dados do paciente
+struct paciente {
+    int codigo;
+    char nome[100];
+    char cpf[15];
+    char nascimento[11];
+    char sexo[2];
+    char telefone[15];
+    char endereco[200];
+    char tipoSanguineo[2];
+    char convenio[50];
+    bool ativo;
+}
+
+main () {
+    
+}
+
